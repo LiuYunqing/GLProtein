a4c18a74d70aa046b72745ff2e2eaa9f28636661
Yunqing Liu <48918246+LiuYunqing@users.noreply.github.com>
2025-08-26 15:25:56 +0800

    Update run_test.py

--- src/benchmark/GNN_PPI/run_test.py
+++ src/benchmark/GNN_PPI/run_test.py
@@ -19,11 +19,11 @@ if __name__ == "__main__":
 
     ppi_path = "data/downstream_datasets/ppi_data/protein.actions.SHS148k.STRING.txt"
     pseq_path = "data/downstream_datasets/ppi_data/protein.SHS148k.sequences.dictionary.tsv"
-    vec_path = "data/downstream_datasets/ppi_data/features/protein_embedding_KeAP20_shs148k.npy"
+    vec_path = "data/downstream_datasets/ppi_data/features/protein_embedding_GLProtein_shs148k.npy"
 
     index_path = "data/downstream_datasets/ppi_data/new_train_valid_index_json/SHS148k.bfs.fold1.json"
     # path to checkpoint
-    dir = "output/ppi/gnn_protbert_148k_bfs_1_2022-08-22\ 13:38:11"
+    dir = "output/ppi/gnn_protbert_148k_bfs_1_2024-03-21\ 14:38:21"
     gnn_model= dir + "/gnn_model_valid_best.ckpt"
 
     test_all = "True"
